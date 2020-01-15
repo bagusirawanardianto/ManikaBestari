34a376eac7953720760f98bf0a9f5b3c7a08f85a
JulianShaden <shadenjulian@gmail.com>
2020-01-15 15:47:41 +0700

    nambah police

--- Assets/Scripts/FollowTarget.cs
+++ Assets/Scripts/FollowTarget.cs
@@ -23,7 +23,7 @@ public class FollowTarget : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (player.position.x != transform.position.x && player.position.x > 0 && player.position.x < 55f)
+        if (player.position.x != transform.position.x && player.position.x > 0 && player.position.x < 58f)
         {
             transform.position = Vector3.Lerp(transform.position, new Vector3(player.position.x, transform.position.y, transform.position.z), 0.1f);
         }
